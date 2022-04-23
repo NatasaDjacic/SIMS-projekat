f434eaee616af82d685d762c37e4c0e0093075fe
radojicicBojan <bojanradojicic00@gmail.com>
2022-04-23 18:38:49 +0200

    Menu, different languages (English, Russian)

--- ZdravoKlinika/ZdravoKlinika/UI/ManagerUI/ManagerMainWindow.xaml.cs
+++ ZdravoKlinika/ZdravoKlinika/UI/ManagerUI/ManagerMainWindow.xaml.cs
@@ -19,9 +19,11 @@ namespace ZdravoKlinika.UI.ManagerUI
     /// </summary>
     public partial class ManagerMainWindow : Window
     {
+
         public ManagerMainWindow()
         {
             InitializeComponent();
+            SwitchLanguage("srb");
             ContentFrame.Navigate(new View.Rooms());
             
         }
@@ -32,6 +34,8 @@ namespace ZdravoKlinika.UI.ManagerUI
                 SerbianMenu.IsChecked = true;
                 EnglishMenu.IsChecked = false;
                 RussianMenu.IsChecked = false;
+
+            SwitchLanguage("srb");
         }
         private void English_Click(object sender, RoutedEventArgs e)
         {
@@ -39,6 +43,29 @@ namespace ZdravoKlinika.UI.ManagerUI
             SerbianMenu.IsChecked = false;
             EnglishMenu.IsChecked = true;
             RussianMenu.IsChecked = false;
+
+            SwitchLanguage("en");
+        }
+
+        private void SwitchLanguage(string languageCode)
+        {
+            ResourceDictionary dictionary = new ResourceDictionary();
+            switch (languageCode)
+            {
+                case "en":
+                    dictionary.Source = new Uri("..\\..\\Resource\\Dictionary\\StringResources.en.xaml", UriKind.Relative);
+                    break;
+                case "rus":
+                    dictionary.Source = new Uri("..\\..\\Resource\\Dictionary\\StringResources.rus.xaml", UriKind.Relative);
+                    break;
+                case "srb":
+                    dictionary.Source = new Uri("..\\..\\Resource\\Dictionary\\StringResources.xaml", UriKind.Relative);
+                    break;
+                default:
+                    dictionary.Source = new Uri("..\\..\\Resource\\Dictionary\\StringResources.xaml", UriKind.Relative);
+                    break;
+            }
+            this.Resources.MergedDictionaries.Add(dictionary);
         }
         private void Russian_Click(object sender, RoutedEventArgs e)
         {
@@ -46,6 +73,9 @@ namespace ZdravoKlinika.UI.ManagerUI
             SerbianMenu.IsChecked = false;
             EnglishMenu.IsChecked = false;
             RussianMenu.IsChecked = true;
+
+            SwitchLanguage("rus");
+
         }
         private void LightTheme_Click(object sender, RoutedEventArgs e)
         {
