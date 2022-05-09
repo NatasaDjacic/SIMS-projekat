b2ae08992568aac0c933d06f54080a6758eac2a4
NatasaDjacic <djacic12@gmail.com>
2022-05-09 21:24:12 +0200

    dijagramIspravke

---
+++ ZdravoKlinika/ZdravoKlinika/Model/Enums/RequestType.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ZdravoKlinika.Model.Enums
+{
+    public enum RequestType
+    {
+        accepted,
+        declined,
+        pending
+    }
+}
