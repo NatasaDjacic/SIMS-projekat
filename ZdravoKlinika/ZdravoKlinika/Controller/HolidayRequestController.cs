52afeb3132a9bb5a0eed1dfd5d85a91cd1e70fe5
NatasaDjacic <djacic12@gmail.com>
2022-05-11 00:05:17 +0200

    requestModel

---
+++ ZdravoKlinika/ZdravoKlinika/Controller/HolidayRequestController.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ZdravoKlinika.Model.Enums;
+using ZdravoKlinika.Service;
+
+namespace ZdravoKlinika.Controller
+{
+    public class HolidayRequestController
+    {
+
+    }
+}
---
+++ ZdravoKlinika/ZdravoKlinika/Model/HolidayRequest.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ZdravoKlinika.Model.Enums;
+
+namespace ZdravoKlinika.Model
+{
+    public class HolidayRequest
+    {
+        public int id { get; set; }
+        public DateTime startTime { get; set; }
+        public DateTime endTime { get; set; }
+        public bool urgency { get; set; }
+        public string reason { get; set; }
+        public RequestType appointmentType { get; set; }
+        public string doctorJMBG { get; set; }
+
+        public string specialization { get; set; }
+    }
+}
